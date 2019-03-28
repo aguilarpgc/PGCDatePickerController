36efdc329111519cb497aa4127b03b7e0c936d51
Paul Aguilar <aguilarpgc@gmail.com>
2019-03-28 16:55:19 -0500

    license / empty example

---
+++ Example/TestDatePickerController/TestDatePickerController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDatePickerController
+//
+//  Created by aguilarpgc on 3/28/19.
+//  Copyright © 2019 aguilarpgc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
